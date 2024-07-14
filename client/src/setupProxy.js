dafe11e7fa9d14fac52754e60762f57367842ade
jisooyu <jisooyu@hotmail.com>
2024-07-14 17:15:11 +0900

    modifying the setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,14 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+
 module.exports = function (app) {
 	const target =
 		process.env.NODE_ENV === 'production'
-			? 'https://https://yu-mern-rtk-query-car-3.vercel.app/'
-			: 'http://localhost:5000';
+			? 'https://your-production-url.com'
+			: 'http://localhost:5555';
 
 	app.use(
 		['/api', '/auth/google', '/car'],
 		createProxyMiddleware({
 			target: target,
+			changeOrigin: true,
 		})
 	);
 };
