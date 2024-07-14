3e0782a4192fea4f2e2c0e92e96fb9a3ee6ff209
jisooyu <jisooyu@hotmail.com>
2024-07-14 19:48:51 +0900

    fixing the url name in setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
 	const target =
 		process.env.NODE_ENV === 'production'
-			? 'https://your-production-url.com'
+			? // ? 'https://your-production-url.com'
+			  'https://yu-mern-rtk-query-car-3-a58sn9uaz-jisooyus-projects.vercel.app/'
 			: 'http://localhost:5555';
 
 	app.use(
