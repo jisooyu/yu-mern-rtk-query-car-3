23fe6218eeb6db983a6d4a158b51d2227b8c977e
jisooyu <jisooyu@hotmail.com>
2024-07-14 20:40:50 +0900

    more changes

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
 	const target =
 		process.env.NODE_ENV === 'production'
 			? // ? 'https://your-production-url.com'
-			  'https://yu-mern-rtk-query-car-3-a58sn9uaz-jisooyus-projects.vercel.app/'
-			: 'http://localhost:5555';
+			  'https://yu-mern-rtk-query-car-3-hs084kb0p-jisooyus-projects.vercel.app/'
+			: 'http://localhost:5000';
 
 	app.use(
 		['/api', '/auth/google', '/car'],
